0b62ddac2d00bced3ca7a61acf930153f3cfa257
djdron <djdron@aeb2dce1-986d-af1f-947f-f1de2426e1a6>
2010-12-09 21:55:50 +0000

    + GPL v3 license text added
    git-svn-id: http://filecopyex.googlecode.com/svn/trunk@12 aeb2dce1-986d-af1f-947f-f1de2426e1a6

--- Framework/FAR/FarPanel.cpp
+++ Framework/FAR/FarPanel.cpp
@@ -1,3 +1,25 @@
+/*
+FileCopyEx - Extended File Copy plugin for Far 2 file manager
+
+Copyright (C) 2004 - 2010 Serge Cheperis aka craZZy
+Bugfixes: slst, CDK, Ivanych, Alter, Axxie and Nsky
+Special thanks to Vitaliy Tsubin
+Far 2 (32 & 64 bit) full unicode version by djdron
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #include "../stdhdr.h"
 #include "farplugin.h"
 #include "farpanel.h"
--- src/FileCopyEx.h
+++ src/FileCopyEx.h
@@ -1,4 +1,27 @@
+/*
+FileCopyEx - Extended File Copy plugin for Far 2 file manager
+
+Copyright (C) 2004 - 2010 Serge Cheperis aka craZZy
+Bugfixes: slst, CDK, Ivanych, Alter, Axxie and Nsky
+Special thanks to Vitaliy Tsubin
+Far 2 (32 & 64 bit) full unicode version by djdron
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #pragma once
+
 #include "../framework/properties.h"
 #include "../framework/far/farplugin.h"
 
