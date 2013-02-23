a08107f391cd8f8803caaf09edbc93d434f5a04e
Ruslan Petrenko <ruslanp@ruslanp.com>
2013-02-22 21:02:06 -0600

    mistype

--- src/Framework/FileUtils.cpp
+++ src/Framework/FileUtils.cpp
@@ -62,7 +62,7 @@ inline String CutEndSlash(const String& v)
 
 inline String AddEndSlash(const String& v)
 {
-  wchar endChar = v[v.len()-1];
+  wchar_t endChar = v[v.len()-1];
   if (endChar =='\\' || endChar == '/')
     return v;
   else
