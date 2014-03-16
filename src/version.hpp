cb8d2044e434708260388f095ad58e7901de3dd5
Michael Lukashov <michael.lukashov@gmail.com>
2014-03-16 12:30:46 +0300

    version info: update

--- src/version.hpp
+++ src/version.hpp
@@ -1,11 +1,12 @@
 #include "sdk/farversion.hpp"
 
 #define PLUGIN_MIN_FAR_VERSION MAKEFARVERSION(3, 0, 0, 2927, VS_RELEASE) // http://api.farmanager.com/ru/whatsnew.html
+#define PLUGIN_VERSION_TXT "3.0.2927"
 #define PLUGIN_MAJOR 0
-#define PLUGIN_BUILD 9
+#define PLUGIN_BUILD 10
 #define PLUGIN_VERSION MAKEFARVERSION(FARMANAGERVERSION_MAJOR, FARMANAGERVERSION_MINOR, 2927, PLUGIN_BUILD, VS_BETA)
 #define PLUGIN_DESC L"Extended File Copy plugin for Far 3 file manager main executable"
 #define PLUGIN_NAME L"FileCopyEx"
 #define PLUGIN_TITLE L"FileCopyEx"
-#define PLUGIN_FILENAME L"FileCopyEx.dll"
-#define PLUGIN_AUTHOR L"Ruslan Petrenko, djdron, craZZy, Max Antipin and other"
+#define PLUGIN_FILENAME L"FileCopyEx3.dll"
+#define PLUGIN_AUTHOR L"Michael Lukashov, Ruslan Petrenko, djdron, craZZy, Max Antipin and other"
