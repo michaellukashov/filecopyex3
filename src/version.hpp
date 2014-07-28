d67b4d0940038b32b67c9f42982ce0cb97c59111
Michael Lukashov <michael.lukashov@gmail.com>
2014-07-28 22:30:25 +0400

    changelog.txt: update

--- src/version.hpp
+++ src/version.hpp
@@ -3,11 +3,11 @@
 #include "sdk/farversion.hpp"
 
 #define PLUGIN_MIN_FAR_VERSION MAKEFARVERSION(3, 0, 0, 2927, VS_RELEASE) // http://api.farmanager.com/ru/whatsnew.html
-#define PLUGIN_VERSION_TXT "3.0.1.11"
+#define PLUGIN_VERSION_TXT "3.0.1.13"
 #define PLUGIN_MAJOR 3
 #define PLUGIN_MINOR 0
 #define PLUGIN_SUBMINOR 1
-#define PLUGIN_BUILD 12
+#define PLUGIN_BUILD 13
 #define PLUGIN_VERSION MAKEFARVERSION(PLUGIN_MAJOR, PLUGIN_MINOR, PLUGIN_SUBMINOR, PLUGIN_BUILD, VS_BETA)
 #define PLUGIN_DESC L"Extended File Copy plugin for Far 3 file manager main executable"
 #define PLUGIN_NAME L"FileCopyEx"
