7a1b0093ad119107428a1fbd443d6c928ccdc692
Michael Lukashov <michael.lukashov@gmail.com>
2014-07-29 17:03:23 +0400

    refactoring: fix includes

--- src/Engine.h
+++ src/Engine.h
@@ -26,7 +26,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include <vector>
 
-#include "copyprogress.h"
+#include "CopyProgress.h"
 #include "Framework/FileNameStoreEnum.h"
 
 struct FileStruct
