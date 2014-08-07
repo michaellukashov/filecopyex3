129c71d83ec6c52fc0db44f58a5437882a56d79d
Igor Yudintsev <igor.yudincev@gmail.com>
2014-08-07 17:44:42 +0400

    removed references to StringList

--- src/Framework/StringVector.h
+++ src/Framework/StringVector.h
@@ -14,6 +14,15 @@ public:
   virtual const String & operator[](size_t i) const { return data[i]; }
   virtual void AddString(const String & v) { data.push_back(v); }
   virtual size_t Count() const { return data.size(); }
+  intptr_t Find(const String & v, intptr_t start = 0) const
+  {
+    for (size_t i = start; i < Count(); ++i)
+    {
+      if (data[i].cmp(v) == 0)
+        return i;
+    }
+    return -1;
+  }
 
 private:
   std::vector<String> data;
