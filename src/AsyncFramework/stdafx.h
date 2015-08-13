73abdb2261c988f88a45430346059a4c0a1caf3e
Michael Lukashov <michael.lukashov@gmail.com>
2015-08-13 20:40:58 +0300

    add AsyncFramework

---
+++ src/AsyncFramework/stdafx.h
@@ -0,0 +1,41 @@
+/***************************************************************************
+ *   Copyright (C) 2009-2010, 2014 by Stefan Fuhrmann                      *
+ *   stefanfuhrmann@alice-dsl.de                                           *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+
+#pragma once
+
+#define NOMINMAX
+
+// Including SDKDDKVer.h defines the highest available Windows platform.
+
+// If you wish to build your application for a previous Windows platform, include WinSDKVer.h and
+// set the _WIN32_WINNT macro to the platform you wish to support before including SDKDDKVer.h.
+
+#include <SDKDDKVer.h>
+
+#define VC_EXTRALEAN            // Exclude rarely-used stuff from Windows headers
+
+#include <windows.h>
+
+#include <assert.h>
+#include <process.h>
+
+#include <vector>
+#include <algorithm>
+
