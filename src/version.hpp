b6004c711268c99fbeeb8c34306711bdf4c9149f
Michael Lukashov <michael.lukashov@gmail.com>
2015-08-13 20:53:47 +0300

    update version info

--- src/version.hpp
+++ src/version.hpp
@@ -3,11 +3,11 @@
 #include "SDK/farversion.hpp"
 
 #define PLUGIN_MIN_FAR_VERSION MAKEFARVERSION(3, 0, 0, 4040, VS_RELEASE) // http://api.farmanager.com/ru/whatsnew.html
-#define PLUGIN_VERSION_TXT "3.0.8.39"
+#define PLUGIN_VERSION_TXT "3.0.8.40"
 #define PLUGIN_MAJOR 3
 #define PLUGIN_MINOR 0
 #define PLUGIN_SUBMINOR 8
-#define PLUGIN_BUILD 39
+#define PLUGIN_BUILD 40
 #define PLUGIN_VERSION MAKEFARVERSION(PLUGIN_MAJOR, PLUGIN_MINOR, PLUGIN_SUBMINOR, PLUGIN_BUILD, VS_BETA)
 #define PLUGIN_DESC L"File Copy Extended plugin for Far 3 file manager main executable"
 #define PLUGIN_NAME L"FileCopyEx3"
